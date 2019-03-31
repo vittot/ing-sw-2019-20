183eea47f5108602c26e85531afba756c59fe84f
MirkoZil <mirko.ziliani@mail.polimi.it>
2019-03-31 17:26:55 +0200

    Kill class update

--- src/main/java/game/model/Kill.java
+++ src/main/java/game/model/Kill.java
@@ -4,5 +4,28 @@ public class Kill {
     private Player killer;
     private Player victim;
     private boolean isRage;
-    //TODO
+
+    public Player getKiller() {
+        return killer;
+    }
+
+    public void setKiller(Player killer) {
+        this.killer = killer;
+    }
+
+    public Player getVictim() {
+        return victim;
+    }
+
+    public void setVictim(Player victim) {
+        this.victim = victim;
+    }
+
+    public boolean isRage() {
+        return isRage;
+    }
+
+    public void setRage(boolean rage) {
+        isRage = rage;
+    }
 }
--- src/main/java/game/model/Map.java
+++ src/main/java/game/model/Map.java
@@ -4,5 +4,36 @@ public class Map {
     private int id;
     private Square[][] grid;
     private int dim1, dim2;
-    //TODO
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Square[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Square[][] grid) {
+        this.grid = grid;
+    }
+
+    public int getDim1() {
+        return dim1;
+    }
+
+    public void setDim1(int dim1) {
+        this.dim1 = dim1;
+    }
+
+    public int getDim2() {
+        return dim2;
+    }
+
+    public void setDim2(int dim2) {
+        this.dim2 = dim2;
+    }
 }
