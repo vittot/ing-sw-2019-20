6d0dbd7207c7c0b6b493b03b9d681536bffa5cb9
vittot <vittot97@gmail.com>
2019-03-31 17:30:54 +0200

    Effect class update

--- src/main/java/game/model/Effect.java
+++ src/main/java/game/model/Effect.java
@@ -1,5 +1,184 @@
 package game.model;
 
 public class Effect {
-    //TODO
+    private int damage;
+    private int marks;
+    private int minEnemy; //minimum number of enemies to hit
+    private int maxEnemy; //maximum number of enemies to hit
+    private boolean moveEnemy; //if the enemy has to be moved
+    private boolean moveShooter; //if the player has to be moved
+    private int minMove; //minimum number of movement of the shooter/enemy
+    private int maxMove; //maximum number of movement of the shooter/enemy
+    private int visibility; //0=enemy has to be visible, 1=enemy has to be not visible, 2=enemy can be visible or not
+    private boolean visibilityAfter; //if the enemy must be visible after the movement effect
+    private int minDist;
+    private int maxDist;
+    private Player shooter;
+    private int cardDir; //{0,1,2,3} identify a direction for the effect
+    private boolean chainTarget; //if the target should be found from the target of the last effect (eg: T.H.O.R.)
+    private boolean prevTarget; //if the target has to be the last hit by the weapon
+    private boolean chainMove; //if the target has to be moved in the position of the last target (eg. Vortex Cannon)
+    private boolean differentTarget; //if the target has to be different from all the previous target of the weapon
+    private boolean sameDirection; //if the target has to be on same direction of the last target
+    private boolean beforeBase; //if the effect is not base effect but can be used before the base effect
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public int getMinEnemy() {
+        return minEnemy;
+    }
+
+    public void setMinEnemy(int minEnemy) {
+        this.minEnemy = minEnemy;
+    }
+
+    public int getMaxEnemy() {
+        return maxEnemy;
+    }
+
+    public void setMaxEnemy(int maxEnemy) {
+        this.maxEnemy = maxEnemy;
+    }
+
+    public boolean isMoveEnemy() {
+        return moveEnemy;
+    }
+
+    public void setMoveEnemy(boolean moveEnemy) {
+        this.moveEnemy = moveEnemy;
+    }
+
+    public boolean isMoveShooter() {
+        return moveShooter;
+    }
+
+    public void setMoveShooter(boolean moveShooter) {
+        this.moveShooter = moveShooter;
+    }
+
+    public int getMinMove() {
+        return minMove;
+    }
+
+    public void setMinMove(int minMove) {
+        this.minMove = minMove;
+    }
+
+    public int getMaxMove() {
+        return maxMove;
+    }
+
+    public void setMaxMove(int maxMove) {
+        this.maxMove = maxMove;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public boolean isVisibilityAfter() {
+        return visibilityAfter;
+    }
+
+    public void setVisibilityAfter(boolean visibilityAfter) {
+        this.visibilityAfter = visibilityAfter;
+    }
+
+    public int getMinDist() {
+        return minDist;
+    }
+
+    public void setMinDist(int minDist) {
+        this.minDist = minDist;
+    }
+
+    public int getMaxDist() {
+        return maxDist;
+    }
+
+    public void setMaxDist(int maxDist) {
+        this.maxDist = maxDist;
+    }
+
+    public Player getShooter() {
+        return shooter;
+    }
+
+    public void setShooter(Player shooter) {
+        this.shooter = shooter;
+    }
+
+    public int getCardDir() {
+        return cardDir;
+    }
+
+    public void setCardDir(int cardDir) {
+        this.cardDir = cardDir;
+    }
+
+    public boolean isChainTarget() {
+        return chainTarget;
+    }
+
+    public void setChainTarget(boolean chainTarget) {
+        this.chainTarget = chainTarget;
+    }
+
+    public boolean isPrevTarget() {
+        return prevTarget;
+    }
+
+    public void setPrevTarget(boolean prevTarget) {
+        this.prevTarget = prevTarget;
+    }
+
+    public boolean isChainMove() {
+        return chainMove;
+    }
+
+    public void setChainMove(boolean chainMove) {
+        this.chainMove = chainMove;
+    }
+
+    public boolean isDifferentTarget() {
+        return differentTarget;
+    }
+
+    public void setDifferentTarget(boolean differentTarget) {
+        this.differentTarget = differentTarget;
+    }
+
+    public boolean isSameDirection() {
+        return sameDirection;
+    }
+
+    public void setSameDirection(boolean sameDirection) {
+        this.sameDirection = sameDirection;
+    }
+
+    public boolean isBeforeBase() {
+        return beforeBase;
+    }
+
+    public void setBeforeBase(boolean beforeBase) {
+        this.beforeBase = beforeBase;
+    }
 }
