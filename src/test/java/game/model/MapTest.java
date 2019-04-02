635608fb552eee0598ee754434277a4e1a9b97b9
vittot <vittot97@gmail.com>
2019-04-02 12:17:48 +0200

    updating square

--- src/test/java/game/model/MapTest.java
+++ src/test/java/game/model/MapTest.java
@@ -6,23 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
 
 class MapTest {
 
+    private Map map;
+
+    @Before
+    public void before()  {
+        map = new Map(1,3,1);
+        Square[][] grid = new Square[3][1];
+        Edge[] edges1 = new Edge[]{Edge.WALL,Edge.WALL,Edge.OPEN,Edge.WALL};
+        Edge[] edges2 = new Edge[]{Edge.OPEN,Edge.WALL,Edge.DOOR,Edge.WALL};
+        Edge[] edges3 = new Edge[]{Edge.DOOR,Edge.WALL,Edge.WALL,Edge.WALL};
+        grid[0][0] = new Square(Color.BLUE, false, 0, 0, map, edges1);
+        grid[1][0] = new Square(Color.BLUE, false, 0, 1, map, edges2);
+        grid[2][0] = new Square(Color.RED, false, 0, 2, map, edges3);
+        map.setGrid(grid);
+    }
+
     @Test
     void getRoom() {
-        Map m = new Map(1,3,1);
-        Square[][] grid = new Square[3][1];
-        grid[0][0] = new Square(Color.BLUE);
-        grid[1][0] = new Square(Color.BLUE);
-        grid[2][0] = new Square(Color.RED);
 
-        m.setGrid(grid);
+        List<Square> blueRoom = new ArrayList<>();
+        blueRoom.add(map.getGrid()[0][0]);
+        blueRoom.add(map.getGrid()[1][0]);
 
-        List<Square> blueRoom = new ArrayList<Square>();
-        blueRoom.add(grid[0][0]);
-        blueRoom.add(grid[1][0]);
-
-        assertEquals(m.getRoom(Color.BLUE), blueRoom);
+        assertEquals(map.getRoom(Color.BLUE), blueRoom);
     }
+
+    @Test
+    void getVisibleTargets() {
+
+
+    }
+
 }
