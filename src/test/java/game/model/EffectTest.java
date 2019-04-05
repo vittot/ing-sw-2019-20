289e6b39c045770e2e76d056742a2a7996ba21f0
MirkoZil <mirko.ziliani@mail.polimi.it>
2019-04-05 15:48:45 +0200

    uml update

---
+++ src/test/java/game/model/EffectTest.java
@@ -0,0 +1,7 @@
+package game.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EffectTest {
+
+}
---
+++ src/test/java/game/model/KillTest.java
@@ -0,0 +1,32 @@
+package game.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KillTest {
+
+    @Test
+    void getKiller() {
+    }
+
+    @Test
+    void setKiller() {
+    }
+
+    @Test
+    void getVictim() {
+    }
+
+    @Test
+    void setVictim() {
+    }
+
+    @Test
+    void isRage() {
+    }
+
+    @Test
+    void setRage() {
+    }
+}
